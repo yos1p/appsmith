d176a2a00a90b3603d6539e7c7e1021f937e03d5
yos1p <yosi.p@alltitudes.com>
2023-03-20 12:18:52 +0700

    Remove logo from login screen

--- app/client/src/pages/UserAuth/Container.tsx
+++ app/client/src/pages/UserAuth/Container.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
 import FooterLinks from "./FooterLinks";
-import { getTenantConfig } from "@appsmith/selectors/tenantSelectors";
 
 type ContainerProps = {
   title: string;
@@ -14,12 +11,9 @@ type ContainerProps = {
 
 function Container(props: ContainerProps) {
   const { children, footer, subtitle, title } = props;
-  const tenantConfig = useSelector(getTenantConfig);
-
   return (
     <div className="flex flex-col items-center gap-4 my-auto min-w-min">
       <div className="bg-white border border-t-4 border-t-[color:var(--ads-color-brand)] py-8 px-6 w-[min(400px,80%)] flex flex-col gap-6 t--login-container">
-        <img className="h-8 mx-auto" src={tenantConfig.brandLogoUrl} />
         <div className="flex flex-col gap-2 text-center">
           <h1 className="text-xl font-semibold text-center">{title}</h1>
           {subtitle && <p className="text-base text-center">{subtitle}</p>}
