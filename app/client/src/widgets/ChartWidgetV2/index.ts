70dcb64870f20aba122f45d05f1cdbcf0b21c90a
Arya Wicaksana <wicaksana94@gmail.com>
2023-04-24 10:49:50 +0700

    Resize canvas size

--- app/client/src/widgets/ChartWidgetV2/index.ts
+++ app/client/src/widgets/ChartWidgetV2/index.ts
@@ -9,8 +9,8 @@ export const CONFIG = {
   isCanvas: false, // Defines if this widget has a canvas within in which we can drop other widgets
   defaults: {
     widgetName: "Chart",
-    rows: 28,
-    columns: 24,
+    rows: 25,
+    columns: 45,
     version: 1,
     chartName: "Profit Report",
     data: {
