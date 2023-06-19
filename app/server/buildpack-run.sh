cdddae27064e5d832bcfea79545c9d20031751ba
yos1p <yosi.p@alltitudes.com>
2023-06-19 13:44:33 +0800

    Change some config files

--- app/server/buildpack-run.sh
+++ app/server/buildpack-run.sh
@@ -3,9 +3,7 @@ mkdir -p dist/plugins
 mkdir -p plugins
 
 # Copy the server jar
-cp ./appsmith-server/target/server-1.0-SNAPSHOT.jar dist/
-cp ./appsmith-server/target/server-1.0-SNAPSHOT.jar ./
+cp -v ./appsmith-server/target/server-*.jar dist/
 
 # Copy all the plugins
-cp ./appsmith-plugins/*/target/*.jar dist/plugins/
-cp ./appsmith-plugins/*/target/*.jar plugins/
+rsync -av --exclude "original-*.jar" ./appsmith-plugins/*/target/*.jar dist/plugins/
